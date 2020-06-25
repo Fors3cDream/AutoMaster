61d750d2e602b4feb78472025d49cccec34ab060
root <root@growing-path-2.localdomain>
2020-06-25 22:09:05 +0800

    Save word2vec to datasets

--- run.py
+++ run.py
@@ -9,15 +9,15 @@ BASE_DIR = os.path.dirname(__file__)
 def run():
     # step1: 预处理数据
     print("Step1: 进行数据预处理")
-    parse_data('{}/datasets/AutoMaster_TrainSet.csv'.format(BASE_DIR),'{}/datasets/AutoMaster_TestSet.csv'.format(BASE_DIR))
+    # parse_data('{}/datasets/AutoMaster_TrainSet.csv'.format(BASE_DIR),'{}/datasets/AutoMaster_TestSet.csv'.format(BASE_DIR))
     
     # step2: 构建字典文件
     print("Step2: 构建字典文件")
-    lines = read_data('{}/datasets/train_set.seg_x.txt'.format(BASE_DIR),
-                      '{}/datasets/train_set.seg_y.txt'.format(BASE_DIR),
-                      '{}/datasets/test_set.seg_x.txt'.format(BASE_DIR))
-    vocab, _ = build_vocab(lines)
-    save_word_dict(vocab, '{}/datasets/vocab.txt'.format(BASE_DIR))
+    # lines = read_data('{}/datasets/train_set.seg_x.txt'.format(BASE_DIR),
+    #                   '{}/datasets/train_set.seg_y.txt'.format(BASE_DIR),
+    #                   '{}/datasets/test_set.seg_x.txt'.format(BASE_DIR))
+    # vocab, _ = build_vocab(lines)
+    # save_word_dict(vocab, '{}/datasets/vocab.txt'.format(BASE_DIR))
     
     # step3: 构建词向量并保存
     print("Step3:构建词向量并保存到文件")
@@ -25,7 +25,8 @@ def run():
           '{}/datasets/train_set.seg_y.txt'.format(BASE_DIR),
           '{}/datasets/test_set.seg_x.txt'.format(BASE_DIR),
           out_path='{}/datasets/word2vec.txt'.format(BASE_DIR),
-          sentence_path='{}/datasets/sentences.txt'.format(BASE_DIR))
+          sentence_path='{}/datasets/sentences.txt'.format(BASE_DIR),
+          w2v_bin_path='{}/datasets/w2v.bin'.format(BASE_DIR))
     print("构建词向量完毕！")
     
     
